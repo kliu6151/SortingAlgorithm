f8f91614821565345bca5ae6c50ffa5c9f05e0b5
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2018-11-15 11:23:00 -0500

    Selection

---
+++ src/KevinLiu/Runner.java
@@ -0,0 +1,13 @@
+package KevinLiu;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr = sortingAlgorithms.randIntArr(5);
+        System.out.println(Arrays.toString(arr));
+        sortingAlgorithms.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
---
+++ src/KevinLiu/sortingAlgorithms.java
@@ -0,0 +1,56 @@
+package KevinLiu;
+
+import java.util.Arrays;
+
+public class sortingAlgorithms {
+
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        //This will loop through the entire array, ensuring all the indexes are checked
+        for (int i = 0; i < arr.length; i++)
+        {
+            //This will loop through for each individual number/index
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                //If the number is greater than the next
+                // number, then they will switch
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    public static void selectionSort(int[] arr)
+    {
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            for(int j = i; j<arr.length; j++)
+            {
+                if(arr[j] < arr[j+1])
+                {
+
+                }
+            }
+        }
+    }
+
+    //Creates an array of random integers
+    public static int[] randIntArr(int count)
+    {
+        int[] arr = new int[count];
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int)(Math.random()*10000);
+        }
+        return arr;
+    }
+
+}
